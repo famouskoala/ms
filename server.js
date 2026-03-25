f8add92340b4bd077d10278918d5e89e29a77678
famouskoala <156853044+famouskoala@users.noreply.github.com>
2026-03-24 23:40:47 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname)));
+// Serve static files from current folder
+app.use(express.static(__dirname));
 
+// Force index.html on any route
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port " + PORT);
 });
